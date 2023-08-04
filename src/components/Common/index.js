e6ad9b095b0ed5800944c5d3dabcf48c34b44faf
shubhmjain30 <shubhmjain30@gmail.com>
2023-08-04 22:48:55 +0530

    refactor: index file added for Common components.

---
+++ src/components/Common/index.js
@@ -0,0 +1,13 @@
+export { default as Alert } from './Alert';
+export { default as Backcam } from './Backcam';
+export { default as Browse } from './Browse';
+export { default as ButtonGlobal } from './ButtonGlobal';
+export { Camera, Frontcam } from './Camera';
+export { default as Fetching } from './Fetching';
+export { Header } from './Header';
+export { default as InputGlobal } from './InputGlobal';
+export { default as Labelglobal } from './Labelglobal';
+export { default as Modal } from './Modal';
+export { Sidebar } from './Sidebar';
+export { default as Uploadfile } from './Uploadfile';
+export { CamDropzone } from './CamDropzone'
