e512b1016746088b06c20ff6e6621b2ce699e44f
shubhmjain30 <shubhmjain30@gmail.com>
2023-08-05 18:50:09 +0530

    input updated

--- src/components/Common/InputGlobal.tsx
+++ src/components/Common/InputGlobal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Ref, forwardRef } from 'react';
 
 type InputProps = {
     type: string;
@@ -14,22 +14,24 @@ type InputProps = {
     props?: any;
 };
 
-const InputGlobal = ({ type, id, className, placeholder, onChange, value, name, maxLength, max, minLength, ...props }: InputProps) => {
-    const handleChange = (e: any) => {
-        if (maxLength) {
-            if (e.target.value.length <= maxLength) {
-                onChange(e);
-            } else if (e.target.value.length >= minLength) {
-                onChange();
-            }
-        } else {
-            e.preventDefault();
-            onChange(e);
-        }
-    };
+const InputGlobal = forwardRef(({ type = 'text', id, className, placeholder, onChange, value, name, maxLength, max, minLength, ...rest }: InputProps, ref: Ref<HTMLInputElement>) => {
+    console.log('[InputGlobal] value', value);
     return (
-        <input type={type} id={id} className={className} onChange={handleChange} placeholder={placeholder} value={value} {...props} max={max} name={name} maxLength={maxLength} minLength={minLength} />
+        <input
+            type={type}
+            id={id}
+            className={className}
+            onChange={onChange}
+            placeholder={placeholder}
+            value={value}
+            max={max}
+            name={name}
+            maxLength={maxLength}
+            minLength={minLength}
+            ref={ref}
+            {...rest}
+        />
     );
-};
+});
 
 export default InputGlobal;
