94f561707335528c80a6111977bb30d04c5c8aa8
shubhmjain30 <shubhmjain30@gmail.com>
2023-08-28 11:16:12 +0530

    fix: major fixes in Camera Component

--- src/components/Common/Camera/Camera.tsx
+++ src/components/Common/Camera/Camera.tsx
@@ -6,13 +6,6 @@ import ButtonGlobal from '../ButtonGlobal';
 import { useStore } from '../../../store/zustand';
 import { FACING_MODE_ENVIRONMENT, FACING_MODE_USER, resolutions } from './cameraConfig';
 
-const VIDEO_CONSTRAINTS = {
-    audio: false,
-    width: 1280,
-    height: 720,
-    aspectRatio: 0.8
-};
-
 type CameraProps = {
     mediaRecorderRef?: any | null;
     capturing?: boolean;
@@ -28,6 +21,7 @@ type CameraProps = {
 
 /**
  * Camera Component
+ *
  */
 const Camera = ({
     capturing,
@@ -48,78 +42,136 @@ const Camera = ({
     const [flashOn, setFlashOn] = useState<boolean>(false);
     const [resolutionIndex] = useState<number>(0);
     const [camDevices, setCamDevices] = useState<any[]>([]);
-    const [currDeviceId, setCurrDeviceId] = useState<string>();
-    const [videoConstraints, setVideoConstraints] = useState<any>({
-        ...VIDEO_CONSTRAINTS,
-        facingMode: facingMode
+    const [deviceIdx, setDeviceIdx] = useState<number>(0);
+    const [videoConstraints, setVideoConstraints] = useState<Object>({
+        width: 1280,
+        height: 720,
+        aspectRatio: 0.8
     });
 
+    /**
+     * To get all the connected devices
+     */
     const getDevices = useCallback((mediaDevices: any) => {
         const _videoMediaDevices = mediaDevices.filter(({ kind }: any) => kind === 'videoinput');
         const _mediaDevices = _videoMediaDevices.map(({ deviceId, label }: { deviceId: string; label: string }) => ({ deviceId, label }));
         setCamDevices(_mediaDevices);
-        setCurrDeviceId(_mediaDevices?.[0]?.deviceId);
     }, []);
 
-    const detectFlashSupport = useCallback(() => {
-        if (!('ImageCapture' in window)) {
-            // return Promise.resolve(false);
-            setHasFlash(false);
-        }
-        const track: any = getCameraTrack();
-        if (track) {
-            const imageCapture = new (window as any).ImageCapture(track);
-            imageCapture
-                .getPhotoCapabilities()
-                .then((result: any) => {
-                    setHasFlash(result.fillLightMode.includes('flash') ? true : false);
-                })
-                .catch(() => {
-                    setHasFlash(false);
-                });
-            /* globals ImageCapture: false */
-            navigator.mediaDevices
-                .getUserMedia({ video: true })
-                .then((mediaStream) => {
-                    webcamRef.current.srcObject = mediaStream;
-                })
-                .catch((error) => console.error(error));
-        } else {
-            console.log(':::Cam No camera track found:::');
-        }
-    }, [setHasFlash]);
+    /**
+     * Initializing Camera
+     */
+    const initCamera = (resolutionIndex: number = 0) => {
+        const res = resolutions[resolutionIndex];
+        const _deviceId = camDevices?.[deviceIdx]?.deviceId
+        setVideoConstraints((prev) => ({
+            ...prev,
+            width: res.w,
+            height: res.h,
+            facingMode: facingMode,
+            deviceId: _deviceId
+        }));
 
-    const getCameraTrack = () => {
-        navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then((mediaStream) => {
-                webcamRef.current.srcObject = mediaStream;
-                return mediaStream.getVideoTracks()[0];
-            })
-            .catch((error) => console.error(error));
+        // if (!cameraStatus) {
+        //     navigator.mediaDevices
+        //         .getUserMedia({
+        //             audio: false,
+        //             video: {
+        //                 ...videoConstraints
+        //             }
+        //         })
+        //         /* Will request permission from user */
+        //         .then((mediaStream) => {
+        //             /* Permission Granted */
+        //             setCameraStatus(true);
+
+        //             webcamRef.current.srcObject = mediaStream;
+
+        //             // detectFlashSupport(); /* temporary commenting this */
+
+        //             // const _videoTracks = mediaStream.getVideoTracks()[0];
+        //             // setVideoTrack(_videoTracks);
+        //         })
+        //         .catch((error) => {
+        //             /* Permission Denied */
+        //             console.error(error);
+        //             setCameraStatus(false);
+        //             throw error;
+        //         });
+        // }
     };
 
-    const toggleFlash = () => {
-        const track: any = getCameraTrack();
-        if (track) {
-            setFlashOn((flashOn: boolean) => {
-                track.applyConstraints({
-                    advanced: [{ torch: flashOn ? true : false }]
-                });
-                return !flashOn;
-            });
-        }
-    };
-
+    /**
+     * To switch between cameras/facing mode
+     */
     const switchCamera = () => {
-        const _availableDeviceId = camDevices?.map((cd: any) => cd.deviceId);
-        const _currentCamIndex = _availableDeviceId.indexOf(currDeviceId);
-        let _newCamIndex = _currentCamIndex < camDevices.length - 1 ? _currentCamIndex + 1 : 0;
-        let _newCamId = camDevices?.[_newCamIndex]?.deviceId;
-        setCurrDeviceId(_newCamId);
+        setDeviceIdx((prev) => (prev < camDevices.length - 1 ? prev + 1 : 0));
         setFacingMode((prev) => (prev === FACING_MODE_USER ? FACING_MODE_ENVIRONMENT : FACING_MODE_USER));
     };
 
+    /**
+     * To detect Flash
+     */
+    // const detectFlashSupport = useCallback(() => {
+    //     if (!('ImageCapture' in window)) {
+    //         // return Promise.resolve(false);
+    //         setHasFlash(false);
+    //     }
+    //     const track: any = getCameraTrack();
+    //     if (track) {
+    //         const imageCapture = new (window as any).ImageCapture(track);
+    //         imageCapture
+    //             .getPhotoCapabilities()
+    //             .then((result: any) => {
+    //                 setHasFlash(result.fillLightMode.includes('flash') ? true : false);
+    //             })
+    //             .catch(() => {
+    //                 setHasFlash(false);
+    //             });
+    //         /* globals ImageCapture: false */
+    //         navigator.mediaDevices
+    //             .getUserMedia({ video: true })
+    //             .then((mediaStream) => {
+    //                 webcamRef.current.srcObject = mediaStream;
+    //             })
+    //             .catch((error) => console.error(error));
+    //     } else {
+    //         console.log(':::Cam No camera track found:::');
+    //     }
+    // }, [setHasFlash]);
+
+    // const getCameraTrack = () => {
+    //     return navigator.mediaDevices
+    //         .getUserMedia({ video: true })
+    //         .then((mediaStream) => {
+    //             console.debug('mediaStream', mediaStream);
+    //             webcamRef.current.srcObject = mediaStream;
+    //             return mediaStream.getVideoTracks()[0];
+    //         })
+    //         .catch((error) => {
+    //             console.error(error);
+    //             throw error; // Re-throw the error to propagate it if needed
+    //         });
+    // };
+
+    /**
+     * To toggle flash
+     */
+    // const toggleFlash = () => {
+    //     const track: any = getCameraTrack();
+    //     if (track) {
+    //         setFlashOn((flashOn: boolean) => {
+    //             track.applyConstraints({
+    //                 advanced: [{ torch: flashOn ? true : false }]
+    //             });
+    //             return !flashOn;
+    //         });
+    //     }
+    // };
+
+    /**
+     * To capture image
+     */
     const capture = useCallback(
         async (e: any) => {
             e.preventDefault();
@@ -133,35 +185,49 @@ const Camera = ({
     );
 
     /**
-     * Initializing Camera with effects
+     * To close camera
      */
-    const initCamera = (resolutionIndex: number = 0) => {
-        const res = resolutions[resolutionIndex];
-        setVideoConstraints({
-            ...videoConstraints,
-            width: res.w,
-            height: res.h,
-            deviceId: currDeviceId
-        });
-    };
-
     const closeCamera = () => {
         setCameraStatus(false);
     };
 
     useEffect(() => {
-        navigator.mediaDevices.enumerateDevices().then(getDevices);
+        navigator.mediaDevices
+            .enumerateDevices()
+            .then(getDevices)
+            .catch((err) => console.error('[Camera] error: Devices not found', err));
     }, []);
 
     useEffect(() => {
-        detectFlashSupport();
-    }, [currDeviceId, facingMode]);
-
-    useEffect(() => {
-        if (webcamRef?.current) {
-            initCamera(resolutionIndex);
+        if (camDevices.length > 0) {
+            initCamera();
         }
-    }, [webcamRef?.current, facingMode]);
+    }, [deviceIdx, camDevices]);
+
+    // useEffect(() => {
+    //     const constraints = {
+    //         facingMode: preferredFacingMode === FACING_MODE_USER ? FACING_MODE_USER : FACING_MODE_ENVIRONMENT
+    //     };
+    //     const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+    //     console.debug('supportedConstraints', supportedConstraints);
+    //     const userMedia = navigator.mediaDevices
+    //         .getUserMedia({ video: constraints })
+    //         .then((mediaStream) => {
+    //             console.debug('mediaStream', mediaStream);
+    //             return mediaStream;
+    //         })
+    //         .catch((error) => console.error(error));
+    // }, [currDeviceId]);
+
+    // useEffect(() => {
+    //     detectFlashSupport();
+    // }, [currDeviceId, facingMode]);
+
+    // useEffect(() => {
+    //     if (webcamRef?.current) {
+    //         initCamera(resolutionIndex);
+    //     }
+    // }, [webcamRef?.current, facingMode]);
 
     const btnClasses = 'bg-curtain text-white border-2 border-white text-[12px] rounded-[6px] px-4';
 
@@ -190,11 +256,11 @@ const Camera = ({
                     <ButtonGlobal onClick={closeCamera} className={btnClasses}>
                         Cancel
                     </ButtonGlobal>
-                    {hasFlash && (
+                    {/* {hasFlash && (
                         <ButtonGlobal onClick={toggleFlash} className={btnClasses}>
                             <>Turn Flash {flashOn ? 'Off' : 'On'}</>
                         </ButtonGlobal>
-                    )}
+                    )} */}
                     {camDevices?.length > 1 && (
                         <ButtonGlobal onClick={switchCamera} className={btnClasses}>
                             Switch Camera
