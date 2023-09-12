a0c2d633a922d8d3c6532c01e97a6938956f1a97
shubhmjain30 <shubhmjain30@gmail.com>
2023-09-12 16:58:40 +0530

    fix: camera component fixed

--- src/components/Common/Camera/Camera.tsx
+++ src/components/Common/Camera/Camera.tsx
@@ -4,7 +4,21 @@ import filledcamera from '../../../assets/icons/filledcamera.svg';
 import ButtonGlobal from '../ButtonGlobal';
 // import retry from '../../assets/icons/retry.png';
 import { useStore } from '../../../store/zustand';
-import { FACING_MODE_ENVIRONMENT, FACING_MODE_USER, resolutions } from './cameraConfig';
+import { FACING_MODE_USER, resolutions } from './cameraConfig';
+
+// camera priority list for web-cams [[ list of keywords (small-cased) ]], this will be preferred over preferredFacingMode
+const CAMERA_PRIORITY: { [key: string]: boolean } = {
+    webcam: true,
+    droidcam: true,
+    source: true
+};
+
+const PREFERRED_FACING_MODE_MAPPING: { [key: string]: string } = {
+    user: 'front',
+    environment: 'back'
+};
+
+type DeviceList = { label: string; deviceId: string; read?: boolean }[];
 
 type CameraProps = {
     mediaRecorderRef?: any | null;
@@ -21,25 +35,10 @@ type CameraProps = {
 
 /**
  * Camera Component
- *
  */
-const Camera = ({
-    // capturing,
-    // setCapturing,
-    // mediaRecorderRef,
-    // recordedChunks,
-    // setRecordedChunks,
-    //imagesVal,
-    type,
-    handleImageCapture,
-    cameraType,
-    preferredFacingMode = FACING_MODE_USER
-}: CameraProps) => {
+const Camera = ({ type, handleImageCapture, cameraType, preferredFacingMode = FACING_MODE_USER }: CameraProps) => {
     const webcamRef = useRef<any | null>(null);
     const { setCameraStatus } = useStore();
-    const [facingMode, setFacingMode] = useState<CameraProps['preferredFacingMode']>(preferredFacingMode);
-    // const [hasFlash, setHasFlash] = useState<boolean>(false);
-    // const [flashOn, setFlashOn] = useState<boolean>(false);
     const [resolutionIndex] = useState<number>(0);
     const [camDevices, setCamDevices] = useState<any[]>([]);
     const [deviceIdx, setDeviceIdx] = useState<number>(0);
@@ -55,11 +54,61 @@ const Camera = ({
     const getDevices = useCallback(
         (mediaDevices: any) => {
             const _videoMediaDevices = mediaDevices.filter(({ kind }: any) => kind === 'videoinput');
-            const _mediaDevices = _videoMediaDevices.map(({ deviceId, label }: { deviceId: string; label: string }) => ({ deviceId, label }));
-            setCamDevices(_mediaDevices);
+            const _mediaDevices: DeviceList = _videoMediaDevices.map(({ label, deviceId }: { label: string; deviceId: string }) => ({
+                label,
+                deviceId,
+                read: false
+            }));
+
+            if (_mediaDevices.length === 1) {
+                setCamDevices(_mediaDevices);
+            } else {
+                const _updatedMediaDevices: DeviceList = getModifiedDeviceList(_mediaDevices);
+                setCamDevices(_updatedMediaDevices);
+            }
         },
         [setCamDevices]
     );
+
+    /**
+     * Function to get camera device id based on different conditions
+     */
+    const getModifiedDeviceList = (_mediaDevices: DeviceList) => {
+        const _deviceList: DeviceList = [];
+
+        _mediaDevices?.forEach((device) => {
+            const _label = device.label.toLowerCase();
+
+            const _splitLabels = _label.split(' ');
+
+            //check if contains any priority keyword
+            for (let ele of _splitLabels) {
+                if (CAMERA_PRIORITY[ele]) {
+                    _deviceList.unshift(device);
+                    device.read = true;
+                    break;
+                }
+                // preferred camera back/rear
+                else if (ele === PREFERRED_FACING_MODE_MAPPING[preferredFacingMode]) {
+                    _deviceList.push(device);
+                    device.read = true;
+                    break;
+                }
+            }
+        });
+
+        // to push devices which are neither in priority nor preferred
+        _mediaDevices.forEach((device) => {
+            if (!device.read) {
+                _deviceList.push(device);
+            }
+
+            delete device.read;
+        });
+
+        console.log('_deviceList', _deviceList);
+        return _deviceList;
+    };
 
     /**
      * Initializing Camera
@@ -71,7 +120,6 @@ const Camera = ({
             ...prev,
             width: res.w,
             height: res.h,
-            facingMode: facingMode,
             deviceId: _deviceId
         }));
     };
@@ -81,68 +129,7 @@ const Camera = ({
      */
     const switchCamera = () => {
         setDeviceIdx((prev) => (prev < camDevices?.length - 1 ? prev + 1 : 0));
-        setFacingMode((prev) => (prev === FACING_MODE_USER ? FACING_MODE_ENVIRONMENT : FACING_MODE_USER));
     };
-
-    /**
-     * To detect Flash
-     */
-    // const detectFlashSupport = useCallback(() => {
-    //     if (!('ImageCapture' in window)) {
-    //         // return Promise.resolve(false);
-    //         setHasFlash(false);
-    //     }
-    //     const track: any = getCameraTrack();
-    //     if (track) {
-    //         const imageCapture = new (window as any).ImageCapture(track);
-    //         imageCapture
-    //             .getPhotoCapabilities()
-    //             .then((result: any) => {
-    //                 setHasFlash(result.fillLightMode.includes('flash') ? true : false);
-    //             })
-    //             .catch(() => {
-    //                 setHasFlash(false);
-    //             });
-    //         /* globals ImageCapture: false */
-    //         navigator.mediaDevices
-    //             .getUserMedia({ video: true })
-    //             .then((mediaStream) => {
-    //                 webcamRef.current.srcObject = mediaStream;
-    //             })
-    //             .catch((error) => console.error(error));
-    //     } else {
-    //         console.log(':::Cam No camera track found:::');
-    //     }
-    // }, [setHasFlash]);
-
-    // const getCameraTrack = () => {
-    //     return navigator.mediaDevices
-    //         .getUserMedia({ video: true })
-    //         .then((mediaStream) => {
-    //             console.debug('mediaStream', mediaStream);
-    //             webcamRef.current.srcObject = mediaStream;
-    //             return mediaStream.getVideoTracks()[0];
-    //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //             throw error; // Re-throw the error to propagate it if needed
-    //         });
-    // };
-
-    /**
-     * To toggle flash
-     */
-    // const toggleFlash = () => {
-    //     const track: any = getCameraTrack();
-    //     if (track) {
-    //         setFlashOn((flashOn: boolean) => {
-    //             track.applyConstraints({
-    //                 advanced: [{ torch: flashOn ? true : false }]
-    //             });
-    //             return !flashOn;
-    //         });
-    //     }
-    // };
 
     /**
      * To capture image
@@ -176,7 +163,7 @@ const Camera = ({
 
     return (
         <div className={'fixed top-0 left-0 right-0 bottom-0 z-50 bg-curtain lg:p-5 p-2 flex flex-col justify-center items-center'}>
-            {/* ${type === 'Pan' || type === 'videoRecord' ? 'flex justify-end' : 'flex'} */}
+            <p className="p-2 text-white border-2 border-white border-solid rounded-lg">{camDevices[deviceIdx]?.label}</p>
             <div className="flex flex-col mr-3 mt-[65px] w-full max-w-[800px] max-h-[95%]">
                 <Webcam
                     className="rounded-[10px]"
@@ -188,7 +175,7 @@ const Camera = ({
                     screenshotQuality={0.98}
                     forceScreenshotSourceSize={true}
                     imageSmoothing={true}
-                    mirrored={camDevices?.length > 1 ? false : true}
+                    mirrored={camDevices?.length === 1 ? true : false}
                     videoConstraints={videoConstraints}
                     onUserMedia={() => {
                         navigator.mediaDevices
@@ -201,6 +188,7 @@ const Camera = ({
                         initCamera(resolutionIndex > resolutions.length ? 0 : resolutionIndex + 1);
                     }}
                 />
+                {/* <p className="text-white bg-primary"> Cam: {camDevices[deviceIdx]?.label}</p> */}
                 <div className={`fixed left-0 right-0 bottom-[10px] lg:bottom-[20px] flex flex-row flex-wrap gap-2 justify-around mt-3 w-full`}>
                     <ButtonGlobal onClick={closeCamera} className={btnClasses}>
                         Cancel
@@ -225,20 +213,5 @@ const Camera = ({
         </div>
     );
 };
-
-/* ) : (
-    type !== 'Aadhaar' && (
-        <>
-            <img src={image} alt="screenshot" className="rounded-[10px]" />
-            <span className="flex justify-end mt-3">
-                <ButtonGlobal onClick={handleRetake} className="text-[12px] p-1 rounded-[4px] w-[10rem]">
-                    <>
-                        <img src={retry} alt="retry_icon" className="w-[16px] h-[16px] mr-1" /> Re-Capture
-                    </>
-                </ButtonGlobal>
-            </span>
-        </>
-    )
-)} */
 
 export default Camera;
