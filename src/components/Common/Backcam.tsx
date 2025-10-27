feda78d76d9092b8e91acfbba718b1ef1a106b53
shubhmjain30 <shubhmjain30@gmail.com>
2025-10-27 15:04:01 +0530

    refactor: improve layout and styling consistency in Backcam and Frontcam components

--- src/components/Common/Backcam.tsx
+++ src/components/Common/Backcam.tsx
@@ -8,11 +8,11 @@ type BackcamProps = {
 };
 const Backcam = ({ handleRetake, imageVal }: BackcamProps) => {
     return (
-        <div className="flex flex-col w-[50%] md:w-[100%] lg:w-[50%] sm:w-[100%] max-[450px]:w-[100%] max-[640px]:w-[100%] md:mb-2 sm:mb-2  mr-3">
-            <div className="w-full bg-curtain rounded-[10px] justify-center flex h-[190px]">
-                <img src={imageVal} alt="screenshot" className="object-contain rounded-[10px] w-full" />
+        <div className="flex flex-col w-[50%] md:w-[100%] lg:w-[50%] sm:w-[100%] max-[450px]:w-[100%] max-[640px]:w-[100%] md:mb-2 sm:mb-2 mr-3">
+            <div className="w-full rounded-[10px] p-4 justify-center flex h-[190px]">
+                <img src={imageVal} alt="screenshot" className="object-contain rounded-[10px] max-w-lg" />
             </div>
-            <span className="flex flex-col items-end justify-end mt-3">
+            <span className="flex flex-col justify-end items-end mt-3">
                 <ButtonGlobal onClick={handleRetake} className="text-[12px] p-1 rounded-[4px] w-[10rem]">
                     <>
                         <img src={retry} alt="retry_icon" className="w-[16px] h-[16px] mr-1" /> Re-Capture
